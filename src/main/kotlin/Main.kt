c5d0b304afee7f5c7809a4e2ec43227c087b29d6
Virtheon <virtheon@pm.me>
2023-01-29 18:34:41 +0000

    added framework for operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,10 +12,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.math.BigDecimal
 import java.math.MathContext
 import java.math.RoundingMode
 
 data class ButtonValues(val buttonText: String, val onClick: () -> Unit)
+data class Operator(val operatorStr: String, val operationFun: (BigDecimal, BigDecimal) -> BigDecimal)
 
 @Composable
 @Preview
@@ -25,7 +27,10 @@ fun App() {
 		var currentNumber by remember { mutableStateOf(0.toBigDecimal(mathContext)) }
 		var isDecimal by remember { mutableStateOf(false) }
 		var isNegative by remember { mutableStateOf(false) }
-		fun generateNumberOnClick(x: Int): () -> Unit = {
+		var previousNumber: BigDecimal? by remember { mutableStateOf(null) }
+		var operator: Operator? by remember { mutableStateOf(null) }
+
+		fun digitOnClick(x: Int): () -> Unit = {
 			val signedX = (if (isNegative) -x else x).toBigDecimal()
 			val ten = 10.toBigDecimal()
 			currentNumber =
@@ -36,34 +41,67 @@ fun App() {
 				else
 					currentNumber * ten + signedX
 		}
+
+		fun operationOnClick(operatorFunction: Operator): (() -> Unit) = {
+			// TODO perform operation of operator is selected a second time
+			previousNumber = currentNumber
+			currentNumber = 0.toBigDecimal()
+			operator = operatorFunction
+		}
+
 		Column {
 			val possibleExtraNegative = if (isNegative && currentNumber.compareTo(0.toBigDecimal()) == 0) "-" else ""
 			val possibleExtraDot = if (isDecimal && currentNumber.scale() == 0) "." else ""
-			Text(possibleExtraNegative + currentNumber.toString() + possibleExtraDot, fontSize = 50.sp)
-			NumberPad(
-				listOf(
-					ButtonValues("1", generateNumberOnClick(1)),
-					ButtonValues("2", generateNumberOnClick(2)),
-					ButtonValues("3", generateNumberOnClick(3)),
-					ButtonValues("4", generateNumberOnClick(4)),
-					ButtonValues("5", generateNumberOnClick(5)),
-					ButtonValues("6", generateNumberOnClick(6)),
-					ButtonValues("7", generateNumberOnClick(7)),
-					ButtonValues("8", generateNumberOnClick(8)),
-					ButtonValues("9", generateNumberOnClick(9)),
-					ButtonValues("+/-") {
-						currentNumber = -currentNumber
-						isNegative = !isNegative
-					},
-					ButtonValues("0", generateNumberOnClick(0)),
-					ButtonValues(".") {
-						isDecimal = !isDecimal
-						if (!isDecimal) {
-							currentNumber = currentNumber.setScale(0, RoundingMode.DOWN)
-						}
-					},
-				)
+			val topText =
+				if (previousNumber != null && operator != null)
+					previousNumber.toString() + " " + operator!!.operatorStr
+				else
+					""
+
+			Text(
+				topText,
+				color = Color.Gray,
+				fontSize = 30.sp
 			)
+			Text(
+				possibleExtraNegative + currentNumber.toString() + possibleExtraDot,
+				fontSize = 50.sp,
+			modifier = Modifier.fillMaxWidth()
+			)
+			Row {
+				NumberPad(
+					listOf(
+						ButtonValues("1", digitOnClick(1)),
+						ButtonValues("2", digitOnClick(2)),
+						ButtonValues("3", digitOnClick(3)),
+						ButtonValues("4", digitOnClick(4)),
+						ButtonValues("5", digitOnClick(5)),
+						ButtonValues("6", digitOnClick(6)),
+						ButtonValues("7", digitOnClick(7)),
+						ButtonValues("8", digitOnClick(8)),
+						ButtonValues("9", digitOnClick(9)),
+						ButtonValues("+/-") {
+							currentNumber = -currentNumber
+							isNegative = !isNegative
+						},
+						ButtonValues("0", digitOnClick(0)),
+						ButtonValues(".") {
+							isDecimal = !isDecimal
+							if (!isDecimal) {
+								currentNumber = currentNumber.setScale(0, RoundingMode.DOWN)
+							}
+						},
+					)
+				)
+				Operators(
+					listOf(
+						ButtonValues("+", operationOnClick(Operator("+") { x, y -> x + y })),
+						ButtonValues("-", operationOnClick(Operator("-") { x, y -> x - y })),
+						ButtonValues("×", operationOnClick(Operator("×") { x, y -> x * y })),
+						ButtonValues("÷", operationOnClick(Operator("÷") { x, y -> x / y })),
+					)
+				)
+			}
 		}
 	}
 }
@@ -72,19 +110,36 @@ fun App() {
 @Composable
 fun NumberPad(buttons: List<ButtonValues>) {
 	LazyVerticalGrid(
-		modifier = Modifier.fillMaxWidth(0.5f),
+		modifier = Modifier.fillMaxWidth(0.4f),
 		horizontalArrangement = Arrangement.SpaceEvenly,
 		cells = GridCells.Fixed(3)
 	) {
 		items(buttons.size) { index ->
-			Button(
-				modifier = Modifier.padding(vertical = 5.dp, horizontal = 8.dp).aspectRatio(1f),
-				colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
-				onClick = buttons[index].onClick
-			) {
-				Text(buttons[index].buttonText, fontSize = 30.sp)
-			}
+			CalculatorButton(buttons[index].buttonText, buttons[index].onClick)
 		}
+	}
+}
+
+@Composable
+fun Operators(buttons: List<ButtonValues>) {
+	Column(
+		modifier = Modifier.padding(vertical = 50.dp),
+		verticalArrangement = Arrangement.SpaceEvenly,
+	) {
+		buttons.forEach {
+			CalculatorButton(it.buttonText, it.onClick)
+		}
+	}
+}
+
+@Composable
+fun CalculatorButton(text: String, onClick: () -> Unit) {
+	Button(
+		modifier = Modifier.padding(vertical = 5.dp, horizontal = 8.dp).aspectRatio(1f),
+		colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
+		onClick = onClick
+	) {
+		Text(text, fontSize = 30.sp)
 	}
 }
 
