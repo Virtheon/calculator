dcdd534b5070f7391859a04c7307202144b06470
Virtheon <virtheon@pm.me>
2023-02-03 17:30:22 +0000

    streamlined conversion to BigDecimal with .bd, streamlined operationOnClick calls, fixed button size and placement and set window to a good starting size

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
@@ -8,10 +9,12 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import java.math.BigDecimal
 import java.math.MathContext
 import java.math.RoundingMode
@@ -19,11 +22,15 @@ import java.math.RoundingMode
 data class ButtonValues(val buttonText: String, val onClick: () -> Unit)
 data class Operator(val operatorStr: String, val operationFun: (BigDecimal, BigDecimal) -> BigDecimal)
 
+val Int.bd: BigDecimal
+	get() = this.toBigDecimal()
+
 @Composable
 @Preview
 fun App() {
 	MaterialTheme() {
 		var mathContext by remember { mutableStateOf(MathContext(0)) }
+		// TODO pack all this into one variable?
 		var currentNumber by remember { mutableStateOf(0.toBigDecimal(mathContext)) }
 		var isDecimal by remember { mutableStateOf(false) }
 		var isNegative by remember { mutableStateOf(false) }
@@ -31,10 +38,10 @@ fun App() {
 		var operator: Operator? by remember { mutableStateOf(null) }
 
 		fun digitOnClick(x: Int): () -> Unit = {
-			val signedX = (if (isNegative) -x else x).toBigDecimal()
-			val ten = 10.toBigDecimal()
+			val signedX = (if (isNegative) -x else x).bd
+			val ten = 10.bd
 			currentNumber =
-				if (currentNumber == 0.toBigDecimal())
+				if (currentNumber == 0.bd)
 					signedX
 				else if (isDecimal)
 					currentNumber + signedX.scaleByPowerOfTen(-(currentNumber.scale() + 1))
@@ -42,15 +49,20 @@ fun App() {
 					currentNumber * ten + signedX
 		}
 
-		fun operationOnClick(operatorFunction: Operator): (() -> Unit) = {
+		fun operationOnClick(
+			operatorSymbol: String,
+			operatorFunction: (BigDecimal, BigDecimal) -> BigDecimal
+		): (() -> Unit) = {
 			// TODO perform operation of operator is selected a second time
 			previousNumber = currentNumber
-			currentNumber = 0.toBigDecimal()
-			operator = operatorFunction
+			currentNumber = 0.bd
+			operator = Operator(operatorSymbol, operatorFunction)
+			isDecimal = false
+			isNegative = false
 		}
 
 		Column {
-			val possibleExtraNegative = if (isNegative && currentNumber.compareTo(0.toBigDecimal()) == 0) "-" else ""
+			val possibleExtraNegative = if (isNegative && currentNumber.compareTo(0.bd) == 0) "-" else ""
 			val possibleExtraDot = if (isDecimal && currentNumber.scale() == 0) "." else ""
 			val topText =
 				if (previousNumber != null && operator != null)
@@ -66,10 +78,11 @@ fun App() {
 			Text(
 				possibleExtraNegative + currentNumber.toString() + possibleExtraDot,
 				fontSize = 50.sp,
-			modifier = Modifier.fillMaxWidth()
+				modifier = Modifier.fillMaxWidth()
 			)
 			Row {
 				NumberPad(
+					Modifier.weight(3f),
 					listOf(
 						ButtonValues("1", digitOnClick(1)),
 						ButtonValues("2", digitOnClick(2)),
@@ -94,11 +107,21 @@ fun App() {
 					)
 				)
 				Operators(
+					Modifier.weight(1f),
 					listOf(
-						ButtonValues("+", operationOnClick(Operator("+") { x, y -> x + y })),
-						ButtonValues("-", operationOnClick(Operator("-") { x, y -> x - y })),
-						ButtonValues("×", operationOnClick(Operator("×") { x, y -> x * y })),
-						ButtonValues("÷", operationOnClick(Operator("÷") { x, y -> x / y })),
+						ButtonValues("+", operationOnClick("+") { x, y -> x + y }),
+						ButtonValues("-", operationOnClick("-") { x, y -> x - y }),
+						ButtonValues("×", operationOnClick("×") { x, y -> x * y }),
+						ButtonValues("÷", operationOnClick("÷") { x, y -> x / y }),
+						ButtonValues("=") {
+							if (previousNumber != null && operator != null) {
+								currentNumber = operator!!.operationFun(previousNumber!!, currentNumber)
+							}
+							previousNumber = null
+							operator = null
+							isDecimal = false
+							isNegative = false
+						}
 					)
 				)
 			}
@@ -108,34 +131,37 @@ fun App() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun NumberPad(buttons: List<ButtonValues>) {
+fun NumberPad(modifier: Modifier, buttons: List<ButtonValues>) {
 	LazyVerticalGrid(
-		modifier = Modifier.fillMaxWidth(0.4f),
-		horizontalArrangement = Arrangement.SpaceEvenly,
-		cells = GridCells.Fixed(3)
+		modifier = modifier.fillMaxHeight(),
+		cells = GridCells.Fixed(3),
 	) {
 		items(buttons.size) { index ->
-			CalculatorButton(buttons[index].buttonText, buttons[index].onClick)
+			CalculatorButton(
+				buttons[index].buttonText,
+				buttons[index].onClick,
+				// only way i could find to make buttons take up all the screen
+				Modifier.fillParentMaxHeight(1f / (buttons.size / 3))
+			)
 		}
 	}
 }
 
 @Composable
-fun Operators(buttons: List<ButtonValues>) {
+fun Operators(modifier: Modifier, buttons: List<ButtonValues>) {
 	Column(
-		modifier = Modifier.padding(vertical = 50.dp),
-		verticalArrangement = Arrangement.SpaceEvenly,
+		modifier = modifier,
 	) {
 		buttons.forEach {
-			CalculatorButton(it.buttonText, it.onClick)
+			CalculatorButton(it.buttonText, it.onClick, Modifier.weight(1f))
 		}
 	}
 }
 
 @Composable
-fun CalculatorButton(text: String, onClick: () -> Unit) {
+fun CalculatorButton(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
 	Button(
-		modifier = Modifier.aspectRatio(1f).padding(7.dp),
+		modifier = modifier.fillMaxSize().padding(7.dp),
 		colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray),
 		onClick = onClick
 	) {
@@ -144,7 +170,11 @@ fun CalculatorButton(text: String, onClick: () -> Unit) {
 }
 
 fun main() = application {
-	Window(onCloseRequest = ::exitApplication) {
+	Window(
+		onCloseRequest = ::exitApplication,
+		state = rememberWindowState(width = 400.dp, height = 600.dp),
+		title = "Calculator",
+	) {
 		App()
 	}
 }
