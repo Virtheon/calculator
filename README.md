7c72828f9f391942c4c7898538be599e58cfb95d
Virtheon <virtheon@pm.me>
2023-02-06 18:01:29 +0000

    cleaned imports and added build instructions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # calculator
 A simple GUI calculator made with Jetpack Compose.
+
+### Build
+
+```sh
+./gradlew :build
+```
+
+### Run
+
+```sh
+./gradlew :run
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
